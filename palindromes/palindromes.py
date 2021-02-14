6f35004afba35599aabea2413217e859c208807d
joyadauche <joy.ada.mich@gmail.com>
2021-02-15 00:04:07 +0100

    Add palindrome solution two explanation for python

--- palindromes/palindromes.py
+++ palindromes/palindromes.py
@@ -1,14 +1,14 @@
 # 1.
-def isPalindrome(string):
+def isPalindromeOne(string):
     reversed_string = ""
     for s in reversed(range(len(string))):
         reversed_string += string[s]
     return string == reversed_string
 ## call function:
-print(isPalindrome("aba"))
-print(isPalindrome("ojo"))
-print(isPalindrome("bless"))
-print(isPalindrome("a"))
+# print(isPalindromeOne("aba"))
+# print(isPalindromeOne("ojo"))
+# print(isPalindromeOne("bless"))
+# print(isPalindromeOne("a"))
 ## Time Complexity: O(n^2)
 # In python, strings are immutable. Hence, when I add a character to the string, 
 # it means I am recreating the whole string. This operation is done in linear time, O(n).
@@ -18,4 +18,29 @@ print(isPalindrome("a"))
 ## Space Complexity: O(n)
 # n characters are stored in reversed_string. As n increases, so will the space, hence O(n)
 
+# 2.
+def isPalindromeTwo(string):
+    reversed_string = []
+    for s in reversed(range(len(string))):
+        reversed_string.append(string[s])
+    return string == "".join(reversed_string)
+## call function:
+# print(isPalindromeTwo("aba"))
+# print(isPalindromeTwo("ojo"))
+# print(isPalindromeTwo("bless"))
+# print(isPalindromeTwo("a"))
 
+# 3.
+def isPalindromeThree(string):
+    left_index = 0
+    right_index = len(string) - 1
+    while left_index < right_index:
+        if string[left_index] != string[right_index]:
+            return False
+        left_index += 1
+        right_index -= 1
+    return True
+print(isPalindromeThree("aba"))
+print(isPalindromeThree("ojo"))
+print(isPalindromeThree("bless"))
+print(isPalindromeThree("a"))
