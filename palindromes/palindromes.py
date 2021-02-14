09910c03118ba97e62e9e6d1ba3a7515e8f7e6e4
joyadauche <joy.ada.mich@gmail.com>
2021-02-14 23:30:44 +0100

    Add solution one for python

--- palindromes/palindromes.py
+++ palindromes/palindromes.py
@@ -4,7 +4,10 @@ def isPalindrome(string):
     for s in reversed(range(len(string))):
         reversed_string += string[s]
     return string == reversed_string
-
+## call function:
 print(isPalindrome("aba"))
 print(isPalindrome("ojo"))
 print(isPalindrome("bless"))
+print(isPalindrome("a"))
+## explaination:
+
