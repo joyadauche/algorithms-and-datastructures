80e234d7431ece914af6c6b4f3fb67d6773b1f7c
joyadauche <joy.ada.mich@gmail.com>
2021-02-14 23:47:46 +0100

    Add solution one explanation for python

--- palindromes/palindromes.py
+++ palindromes/palindromes.py
@@ -9,5 +9,13 @@ print(isPalindrome("aba"))
 print(isPalindrome("ojo"))
 print(isPalindrome("bless"))
 print(isPalindrome("a"))
-## explaination:
+## Time Complexity: O(n^2)
+# In python, strings are immutable. Hence, when I add a character to the string, 
+# it means I am recreating the whole string. This operation is done in linear time, O(n).
+# which means that as the input size, n, increases, so will the time it takes the algorithm to run.
+# But note that it is done within a loop which runs for n times,
+# Hence, O(n) * n = O(n^2)
+## Space Complexity: O(n)
+# n characters are stored in reversed_string. As n increases, so will the space, hence O(n)
+
 
